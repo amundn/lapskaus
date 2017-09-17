d58f09d63a67dbf929d2117b465e494ed1f0f338
Bjørn Reppen <bjreppen@gmail.com>
2017-09-17 15:44:10 +0200

    Add UI component library #12

--- src/Sample.stories.js
+++ src/Sample.stories.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import {muiTheme} from 'storybook-addon-material-ui'
 
-storiesOf('Button', module)
+import {Paper} from 'material-ui'
+
+storiesOf('Sample', module)
+    .addDecorator(muiTheme())
+    .add('abc', () => (
+            <div style={{margin: '20px'}}>
+                <Paper>abc</Paper>
+                </div>
+        ))
   .add('with text', () => (
     <button onClick={action('clicked')}>Hello Button</button>
   ))
