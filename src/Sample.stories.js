a67be6298128e14821aecc8970c7f5e4f7a43b00
Bjørn Reppen <bjreppen@gmail.com>
2017-09-18 12:53:32 +0200

    Add lint on commit

--- src/Sample.stories.js
+++ src/Sample.stories.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import {muiTheme} from 'storybook-addon-material-ui'
+import { muiTheme } from 'storybook-addon-material-ui'
 
-import {Paper} from 'material-ui'
+import { Paper } from 'material-ui'
 
 storiesOf('Sample', module)
-    .addDecorator(muiTheme())
-    .add('abc', () => (
-            <div style={{margin: '20px'}}>
-                <Paper>abc</Paper>
-                </div>
-        ))
+  .addDecorator(muiTheme())
+  .add('abc', () => (
+    <div style={{ margin: '20px' }}>
+      <Paper>abc</Paper>
+    </div>
+  ))
   .add('with text', () => (
     <button onClick={action('clicked')}>Hello Button</button>
   ))
